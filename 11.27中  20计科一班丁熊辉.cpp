a6fde5f935bf05d0fd593ee7c3c06d68640d5315
DING <74812872+DD123456D@users.noreply.github.com>
2020-11-28 16:05:47 +0800

    Add files via upload

---
+++ 11.27中  20计科一班丁熊辉.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+	float a,b,c;
+	scanf("%d%d%d,&a&b&c");
+	min=a<b?:a:b;
+	printf("min=%d",min)
+	return 0;
+ } 
---
+++ 11.27晚   20计科一班丁熊辉.cpp
@@ -0,0 +1,35 @@
+/*#include <stdio.h>
+int main()
+{
+	float x,y;
+	printf("请输入x的值：");
+	scanf("%f",&x);
+	if (x<1)
+		y=x; 
+	else
+		y=(x<10)?(2*x-1):(3*x-1);
+		printf("y的值为%.2f\n",y);
+} 
+ #include<stdio.h>
+int main()
+{   
+int i,j;
+int a[2][3]={1,2,3,4,5,6},b[3][2];
+printf("input array a:\n");
+for(i=0;i<2;i++)
+{for(j=0;j<3;j++)
+printf("%5d",a[i][j]);
+b[j][i]=a[i][j];
+printf("\n");
+}
+printf("\n");
+printf("input array b\n");
+for(j=0;j<3;j++)
+{for(i=0;i<2;i++)
+printf("%5d",b[j][i]);
+printf("\n");
+}
+printf("\n");
+return 0;
+}*/
+
